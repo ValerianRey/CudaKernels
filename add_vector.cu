f12717288bed44f0adcb0a976769be7811ad61ec
Valérian Rey <valerian.rey@gmail.com>
2025-12-15 18:47:23 +0100

    feat: Add parallelization and memory prefetching in add_vector

--- add_vector.cu
+++ add_vector.cu
@@ -1,16 +1,34 @@
+// Based on the excellent tutorial https://developer.nvidia.com/blog/even-easier-introduction-cuda/
+
 #include <iostream>
 #include <cuda_runtime.h>
 #include <math.h>
 
 __global__ void add(int N, float *sum, float *x, float *y)
 {
-	for (int i = 0; i < N; i++)
+	int index = blockIdx.x * blockDim.x + threadIdx.x;
+	int stride = blockDim.x * gridDim.x;
+
+	for (int i = index; i < N; i+=stride)
 		sum[i] = x[i] + y[i];
 }
 
 int main(void)
 {
 	int N = 1<<20;
+	int blockSize, numBlocks;
+	
+	blockSize = 256;
+	numBlocks = (N + blockSize - 1) / blockSize;
+
+	// prompt user for block size and number of blocks
+	//std::cout<<"Enter the desired block size (threads per block)"<<std::endl;
+	//std::cin>>blockSize;
+	
+	//std::cout<<"Enter the desired number of blocks"<<std::endl;
+	//std::cin>>numBlocks;
+
+	std::cout<<"Computing vector sum with "<<numBlocks<<" blocks of size "<<blockSize<<std::endl;
 
 	float *x, *y, *sum;
 	cudaMallocManaged(&x, N*sizeof(float));
@@ -22,7 +40,11 @@ int main(void)
 		y[i] = 2.0f;
 	}
 
-	add<<<1, 1>>>(N, sum, x, y);
+	cudaMemPrefetchAsync(x, N*sizeof(float), 0, 0);
+	cudaMemPrefetchAsync(y, N*sizeof(float), 0, 0);
+	cudaMemPrefetchAsync(sum, N*sizeof(float), 0, 0);
+
+	add<<<numBlocks, blockSize>>>(N, sum, x, y);
 	
 	cudaDeviceSynchronize();
 
