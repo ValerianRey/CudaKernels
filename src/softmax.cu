3bc8c59f02b9321de0f35c5e8b71d5c0aa523a73
Valérian Rey <valerian.rey@gmail.com>
2026-09-01 02:30:30 +0200

    Avoid summing on cpu

--- src/softmax.cu
+++ src/softmax.cu
@@ -7,7 +7,7 @@
 #define BLOCK_SIZE 256
 
 
-__global__ void sum_exp_reduction_tree(int N, float* input, float* buf) {
+__global__ void sum_exp_reduction_tree(int N, float* input, float* denom) {
     int x = threadIdx.x;
     int b = blockIdx.x * blockDim.x;
     
@@ -27,13 +27,13 @@ __global__ void sum_exp_reduction_tree(int N, float* input, float* buf) {
     }
     __syncthreads();
     if (x == 0) {
-        buf[blockIdx.x] = block_buf[0];
+        atomicAdd(denom, block_buf[0]);
     }
 }
 
-__global__ void inplace_softmax(int N, float* input, float denom_inv) {
+__global__ void inplace_softmax(int N, float* input, const float* denom) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
-    input[i] = input[i] * denom_inv;
+    input[i] = input[i] / *denom;
 }
 
 void softmax_(int N, float* input) {
@@ -43,21 +43,14 @@ void softmax_(int N, float* input) {
 
     int num_blocks = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
 
-    CudaManagedArray<float> buf(num_blocks);
-
-    sum_exp_reduction_tree<<<num_blocks, BLOCK_SIZE>>>(N, input, buf.get());
+    CudaManagedArray<float> denom(1);
+    denom[0] = 0.F;
+    sum_exp_reduction_tree<<<num_blocks, BLOCK_SIZE>>>(N, input, denom.get());
 
     CUDA_CHECK(cudaGetLastError());
 	CUDA_CHECK(cudaDeviceSynchronize());
 
-    float buf_sum = 0.F;
-    for (int i = 0; i < num_blocks; i++) {
-        buf_sum += buf[i];
-    }
-
-    float denom_inv = 1.F / buf_sum;
-
-    inplace_softmax<<<num_blocks, BLOCK_SIZE>>>(N, input, denom_inv);
+    inplace_softmax<<<num_blocks, BLOCK_SIZE>>>(N, input, denom.get());
 
     CUDA_CHECK(cudaGetLastError());
 	CUDA_CHECK(cudaDeviceSynchronize());
