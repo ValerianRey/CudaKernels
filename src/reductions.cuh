03cc90f85b4f14ec51913b4b39a0df4b89d5dc83
Valérian Rey <valerian.rey@gmail.com>
2026-09-02 01:42:34 +0200

    Add extra initial reduction step to max_reduction_kernel

--- src/reductions.cuh
+++ src/reductions.cuh
@@ -5,6 +5,7 @@
 
 
 #define BLOCK_SIZE ((256))
+#define ELEMS_PER_BLOCK  ((BLOCK_SIZE) * 2)
 
 template <typename T>
 __global__ void sum_reduction_kernel(int N, T* input, T* out) {
@@ -59,13 +60,16 @@ T sum_reduction(int N, T* input) {
 template <typename T>
 __global__ void max_reduction_kernel(int N, T* input, T* out) {
     int x = threadIdx.x;
-    int b = blockIdx.x * blockDim.x;
+    int b = blockIdx.x * ELEMS_PER_BLOCK;
     
     __shared__ T buffer[BLOCK_SIZE];
 
-    if (x + b < N) {
+    if (x + b + BLOCK_SIZE < N) {
+        buffer[x] = max(input[x + b], input[x + b + BLOCK_SIZE]);
+    } else if (x + b < N) {
         buffer[x] = input[x + b];
-    } else {
+    }
+    else {
         buffer[x] = *out;
     }
 
@@ -96,7 +100,7 @@ T max_reduction(int N, T* input) {
     CUDA_CHECK(cudaMallocManaged(&out, sizeof(T)));
     *out = std::numeric_limits<T>::lowest();
 
-    int num_blocks = N == 0? 1 : (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    int num_blocks = N == 0? 1 : (N + ELEMS_PER_BLOCK - 1) / ELEMS_PER_BLOCK;
 
     max_reduction_kernel<<<num_blocks, BLOCK_SIZE, /*dynamic shared mem per block=*/0, /*stream=*/0>>>(N, input, out);
     CUDA_CHECK(cudaGetLastError());
