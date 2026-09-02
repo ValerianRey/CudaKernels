3c02343e3341173ca8fb31523c3339280f660e2d
Valérian Rey <valerian.rey@gmail.com>
2026-09-02 12:34:58 +0200

    Add optimized softmax impl

--- src/softmax.cu
+++ src/softmax.cu
@@ -7,60 +7,69 @@
 #define BLOCK_SIZE 256
 
 
-__global__ void sum_exp_reduction_tree(int N, float* input, float* denom) {
+__global__ void sum_exp(int N, float* input, float* out) {
     int x = threadIdx.x;
-    int b = blockIdx.x * blockDim.x;
-    
-    __shared__ float block_buf[BLOCK_SIZE];
-    if (x + b < N) {
-        input[b + x] = exp(input[b + x]);
-        block_buf[x] = input[b + x];
+    int bx = blockIdx.x * blockDim.x;
+
+    __shared__ float shared_exp[BLOCK_SIZE];
+
+    if (bx + x < N) {
+        shared_exp[x] = __expf(input[bx + x]);
     } else {
-        block_buf[x] = 0.F;
+        shared_exp[x] = 0.F;
     }
 
-    for (int stride = BLOCK_SIZE >> 1; stride >= 32; stride >>= 1) {
+    for (int offset = BLOCK_SIZE / 2; offset >= 32; offset /= 2) {
         __syncthreads();
-        if (x < stride) {
-            block_buf[x] += block_buf[x + stride];
+        if (x < offset) {
+            shared_exp[x] += shared_exp[x + offset];
         }
     }
     __syncthreads();
 
     if (x < 32) {
-        float val = block_buf[x];
-        for (int stride = 16; stride > 0; stride >>= 1) {
-            val += __shfl_down_sync(0xffffffff, val, stride);
+        float sum_result = shared_exp[x];
+        for (int offset = 16; offset > 0; offset /= 2) {
+            sum_result += __shfl_down_sync(0xffffffff, sum_result, offset);
         }
         if (x == 0) {
-            atomicAdd(denom, val);
+            atomicAdd(out, sum_result);
         }
     }
 }
 
-__global__ void inplace_softmax(int N, float* input, const float* denom) {
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < N) {
-        input[i] = input[i] / *denom;
+__global__ void divide_pointwise(int N, float* input, float denom) {
+    int x = threadIdx.x;
+    int bx = blockIdx.x * blockDim.x;
+
+    if (bx + x >= N) {
+        return;
     }
+
+    input[bx + x] = __expf(input[bx + x]) / denom;
 }
+
 
 void softmax_(int N, float* input) {
     if (N <= 0) {
         return;
     }
 
+    float* denom = nullptr;
+    cudaMallocManaged(&denom, sizeof(float));
+    *denom = 0.F;
+
     int num_blocks = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
 
-    CudaManagedArray<float> denom(1);
-    denom[0] = 0.F;
-    sum_exp_reduction_tree<<<num_blocks, BLOCK_SIZE>>>(N, input, denom.get());
+    sum_exp<<<num_blocks, BLOCK_SIZE, 0, 0>>>(N, input, denom);
 
-    CUDA_CHECK(cudaGetLastError());
-	CUDA_CHECK(cudaDeviceSynchronize());
+    cudaGetLastError();
+    cudaStreamSynchronize(0);
 
-    inplace_softmax<<<num_blocks, BLOCK_SIZE>>>(N, input, denom.get());
+    divide_pointwise<<<num_blocks, BLOCK_SIZE, 0, 0>>>(N, input, *denom);
 
-    CUDA_CHECK(cudaGetLastError());
-	CUDA_CHECK(cudaDeviceSynchronize());
+    cudaGetLastError();
+    cudaStreamSynchronize(0);
+
+    cudaFree(denom);
 }
